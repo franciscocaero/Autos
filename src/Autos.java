56f8d814d642275d387c3dc74a24d2c16b368698
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2023-11-21 08:11:13 -0500

    a

--- src/Autos.java
+++ src/Autos.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Autos {
+public class Autos {
+    public String marca;
+    public int anio;
+    public String color;
+    public Autos (String nuevaMarca){marca=nuevaMarca;}
+    public String getMarca(){
+        return marca;
+    }
+    public int getAnio(){
+        return anio;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public void setMarca(String newMarca) {
+        this.marca = newMarca;
+    }
+
+    public void setAnio(int newAnio) {
+        this.anio = newAnio;
+    }
+
+    public void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    public void metodo1(){
+
+    }
+    public void metodo2(){
+
+    }
 }
